f9d26f76dc3dd29f0d72eb94245522c37dfd63c8
Alastrantia <niklas.lulay@icloud.com>
2024-09-10 20:24:10 +0200

    Create TurtleCoordinateSytstem.py
    Very basic coordinate system using the turtle module. Not really useful in this case cause there’s just a setpos method, but the concept can be applied to the robot 👍

---
+++ TurtleCoordinateSytstem.py
@@ -0,0 +1,21 @@
+import turtle
+import math
+
+turtle.penup()
+turtle.setpos(0, 0)
+turtle.pendown()
+
+target_x = int(input("target x?"))
+target_y = int(input("target y?"))
+    
+def move_to_target(target_x, target_y):
+    difference_x = target_x - turtle.xcor()
+    difference_y = target_y - turtle.ycor()
+    
+    distance = math.sqrt(difference_x**2 + difference_y**2)
+    angle = math.degrees(math.atan2(difference_y, difference_x))
+    
+    turtle.setheading(angle)
+    turtle.forward(distance)
+
+move_to_target(target_x, target_y)
