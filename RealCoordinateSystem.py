36f2938e28b2285be9bd01a6dbf856746ccbe3d6
Alastrantia <niklas.lulay@icloud.com>
2024-09-11 19:47:48 +0200

    @Grattapitone Ooops it still won’t work just try this
    @Grattapitone

--- RealCoordinateSystem.py
+++ RealCoordinateSystem.py
@@ -28,9 +28,7 @@ class CoordindateSystem():
     def goTo(self, target_x, target_y, speed=300):
         if target_x > self.width or target_y > self.height or target_x < 0 or target_y < 0:
             print("Invalid Coordinates")
-            continue
-            #if it still doesn't work, just remove the entire if-structure
-        
+            sys.exit()
         self.difference_x = target_x - self.x
         self.difference_y = target_y - self.y
         
